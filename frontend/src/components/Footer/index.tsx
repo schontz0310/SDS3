5be35d83381c99d777766f0a22db29f9e88f7f6e
schontz0310 <schontz0310@gmail.com>
2021-05-08 23:30:15 -0300

    basic componentes created

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por
+          <a href="https://github.com/schontz0310" target="_blank" rel="noreferrer">Eliverto S Moraes</a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: 
+            <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@eliverto_s_m</a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
